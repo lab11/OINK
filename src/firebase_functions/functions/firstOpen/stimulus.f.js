66039f8c9f5720bfa555eac5a2521469614ab6ad
Joe Breda <joebreda@1x-nat-vl931-172-30-190-0.wireless.umass.edu>
2018-03-23 16:02:35 -0400

    remade function on Fri Mar 23 16:02:35 EDT 2018

--- src/firebase_functions/functions/firstOpen/stimulus.f.js
+++ src/firebase_functions/functions/firstOpen/stimulus.f.js
@@ -12,27 +12,6 @@ try {admin.initializeApp(functions.config().firebase);} catch(e) {}
 var db = admin.firestore();
 var FieldValue = admin.firestore.FieldValue;
 
-
-//stimulus_firstOpen funtion:
-
-// Triggers upon app first open via first timestamp of account sign in. 
-// Validates first log in by checking that account is never been opened before. 
-// Calculates the amount to be paid to the user and enqueues transaction to tx_core_payment collection. 
-// Otherwise, sets status of the firstOpen_transcation doc to "restricted".
-// If fails, throws error and upadtes the status to "failed".
-
-// - Parameters:
-
-// * Validation bit: bit set to high if account has ever been longed in on device
-// * costFirstOpen: The value paid 
-// * event: Event that triggered the function. In this case this is the new document created by the App.
-/*
-exports = module.exports = functions.https.onRequest((request, response) => {
-    response.send("Hello from Joe");
-})
-
-*/
-
 exports = module.exports = functions.firestore
     .document('firstOpen_transaction/{docId}').onCreate((event)=>{
         const data = event.data.data()
@@ -56,42 +35,5 @@ exports = module.exports = functions.firestore
         })
     })
 
-
-/*
-exports = module.exports = functions.firestore
-    .document('firstOpen_transaction/{docId}').onCreate((event) =>{
-        //Getting the data that was modified and initializing all the parameters for payment.
-        const data = event.data.data();
-        const docId = event.params.docId;
-        const costFirstOpen = 5;
-        var previouslyOpened = false;
-        
-        //console.log(`The onCreate event document is: ${util.inspect(data)}`);
-        console.log(`The docId of the creation was: ${util.inspect(docId)}`);
-
-        return db.collection('firstOpen_transaction').where('user_id','==', data.user_id).get()              
-})
-.then(() => {
-return db.collection('tx_core_payment').add({
-    user_id: data.user_id,
-    amount: toPay,
-    msgs: [],
-    num_attempts: 0,
-    time: FieldValue.serverTimestamp(),
-    type: 'invite',
-    stimulus_doc_id: docId,
-    status: 'pending',
-    reattempt: false
-    
-})
-})
-*/
-
-
-
-
 //https://us-central1-paymenttoy.cloudfunctions.net/generatorsFirstOpen
 
-//validation and build snapshot and amount copy and paste from invite function
-//write to core 1 by checking fields core one uses and filling them in copy and paste invite function
-// console.logs goes to function within console drop down
