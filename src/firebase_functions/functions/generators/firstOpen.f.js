18dddf88e4ee837a91db94cc3c067617e0fcf638
Joe Breda <joebreda@1x-nat-vl931-172-30-190-0.wireless.umass.edu>
2018-03-23 10:53:26 -0400

    “testing on Fri Mar 23 10:53:26 EDT 2018

--- src/firebase_functions/functions/generators/firstOpen.f.js
+++ src/firebase_functions/functions/generators/firstOpen.f.js
@@ -16,7 +16,7 @@ exports = module.exports = functions.https
     .onRequest((req, res) => {
     const reqBody = req.body
     reqBody['time'] = FieldValue.serverTimestamp()
-    reqBody['hello'] = FieldValue.serverTimestamp()
+    //reqBody['hello'] = FieldValue.serverTimestamp()
     var dummyCron = db.collection('firstOpen_transaction').add(reqBody);
     res.status(200).send(reqBody);
 
