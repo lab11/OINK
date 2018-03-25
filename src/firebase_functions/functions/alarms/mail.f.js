5ae5df8b0434f49f0df466aabf7da66e429d9e51
santiagocorrea <santiago.correa.cardona@gmail.com>
2018-03-25 11:25:46 -0400

    adding html on Sun Mar 25 11:25:46 EDT 2018

--- src/firebase_functions/functions/alarms/mail.f.js
+++ src/firebase_functions/functions/alarms/mail.f.js
@@ -45,7 +45,8 @@ exports = module.exports = functions.firestore
                 Details: ${data.reason}
                 Timestamp: ${data.timestamp}.
                 User ID: ${data.user_id}.
-                Alarm ID: ${docId}.`
+                Alarm ID: ${docId}.`,
+            html: '<img src="https://firebasestorage.googleapis.com/v0/b/paymenttoy.appspot.com/o/Screen%20Shot%202018-02-19%20at%204.52.01%20PM.png?alt=media&token=85aeafe0-c985-4a5e-bd52-c9779cfcfcc2">'
         }
         return mailTransport.sendMail(mailOptions).then(()=>{
             console.log(`Email to ${emails.join()} sent.`)
