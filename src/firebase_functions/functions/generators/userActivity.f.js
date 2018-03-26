5080c54550cdd95d5586c0c96b2cad9a0492cd67
santiagocorrea <santiago.correa.cardona@gmail.com>
2018-03-26 15:20:13 -0400

    Adding cron-job input on Mon Mar 26 15:20:13 EDT 2018

---
+++ src/firebase_functions/functions/generators/userActivity.f.js
@@ -0,0 +1,23 @@
+const functions = require('firebase-functions');
+const curl = require('curlrequest');
+const admin = require('firebase-admin');
+const util = require('util');
+const request = require('request-promise');
+const crypto = require('crypto');
+const sortObj = require('sort-object');
+try {admin.initializeApp(functions.config().firebase);} catch(e) {}
+ // You do that because the admin SDK can only be initialized once.
+
+//Creating a firebase object to navigate it:
+var db = admin.firestore();
+var FieldValue = admin.firestore.FieldValue;
+
+exports = module.exports = functions.https
+    .onRequest((req, res) => {
+    const reqBody = req.body
+    reqBody['timestamp'] = new Date().getTime()
+    var dummyDoc = db.collection('user_activity').add(reqBody);
+    res.status(200).send(reqBody);
+
+});
+
