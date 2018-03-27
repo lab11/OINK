78720386e49e5287c6b783a7120e5b28ab4eaa40
santiagocorrea <santiago.correa.cardona@gmail.com>
2018-03-27 14:01:51 -0400

    setting up the function on Tue Mar 27 14:01:51 EDT 2018

--- src/firebase_functions/functions/cron/stimulus.f.js
+++ src/firebase_functions/functions/cron/stimulus.f.js
@@ -18,14 +18,22 @@ exports = module.exports = functions.https
     .onRequest((req, res) => {
         const currentTime = new Date().getTime()
         const lastHalfDay = currentTime - 43200000
+        var dic = new Object();
     
         return db.collection('user_activity').orderBy('timestamp').startAt(lastHalfDay).get() //TODO: Need to add the startAt() part
                 .then(snapshot => {
                     snapshot.forEach(doc => {
-                        console.log(doc.id, '=>', doc.data());
+                        //console.log(doc.id, '=>', doc.data());
+                        if (!(doc.data().user_id in dic)){
+                            dic[doc.data().user_id] = [{timestamp:doc.data().timestamp, status: doc.data().status}]
+                        }
+                        else {
+                            dic[doc.data().user_id].push({timestamp:doc.data().timestamp, status: doc.data().status})
+                        }
                     });
                 })
                 .then(() => {
+                    console.log(dic)
                     res.status(200).send("OK");
                 })
                 .catch(err =>{
