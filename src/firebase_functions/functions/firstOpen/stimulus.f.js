5ef2222b94e7b17ac58820e4e5e380092c358cbb
Joe Breda <joebreda@1x-nat-vl931-172-30-153-104.wireless.umass.edu>
2018-04-01 17:15:38 -0400

    stimulus.f.js = firstOpenStimulus

--- src/firebase_functions/functions/firstOpen/stimulus.f.js
+++ src/firebase_functions/functions/firstOpen/stimulus.f.js
@@ -28,67 +28,41 @@ should I be using .then calls for things that are independent
 */
 
 exports = module.exports = functions.firestore
-    .document('firstOpen_Queue/{docId}').onCreate((event)=>{
+    .document('firstOpen_transaction/{docId}').onCreate((event)=>{
         const docId = event.params.docId
         const data = event.data.data()
-        const costFirstOpen = 5
         const user_id = data.user_id
+        const amount = data.amount
         const imei = data.imei
         const token = data.token
         const currentTimestamp = Firebase.ServerValue.TIMESTAMP
         console.log(`The docId of the creation was: ${util.inspect(docId)}`)
-        // Case 1.0: User is already present in user_list meaning they just reinstalled the app.
-        var user = db.collection('user_list').doc(user_id)
 
-        return user.get().then((doc) => {
-            if(doc.exists) {
-                // Case 1.1 (edge coverage): Just in case of packet loss or double packet being sent and user was already set to active
-                if(doc.data().active){
-                    console.log('The user was already active...')
-                    return
-                } 
-                // Case 1.2: Update user to active & log user_activity
-                else {
-                    return db.collection('user_list').doc(user_id).update({
-                        active: true
-                    })
-                    .then(() => {
-                        return db.collection('user_activity').add({
-                            user_id: user_id,
-                            active: true,
-                            timestamp: currentTimestamp
-                        })
-                    })
-                }
-            }
-            // Case 2.0 User was not present in user_list meaning this is the first time they are ever opening the app
-            else {
-                return db.collection('user_list').doc(user_id).set({
-                    active: true,
-                    imei: imei,
-                    instance_id: "?",
-                    phone_num: "unknown",
-                    timestamp: currentTimestamp,
-                    token: token
-                })
-                .then(() => {
-                    return db.collection('user_activity').add({
-                        user_id: user_id,
-                        active: true,
-                        timestamp: currentTimestamp
-                    })
-                })
-                .then(() => {
-                    return db.collection('user_timers').doc(user_id).set({
-                        cycle: 1,
-                        elapsedTime: 0,
-                        firstOpenTime: 0,
-                        lastTimeActive: 0,
-                        active: true,
-                        timePaidArr: []
-                    })
-                })
-            }
+        return db.collection('firstOpen_transaction').doc(docId).update({
+            user_activity: "active",
+            amount: amount,
+            time_processed: currentTimestamp,
+            processed: true
+        })
+        .then(() => {
+            return db.collection('tx_core_payment').add({
+                user_id: user_id,
+                amount: amount,
+                msgs: [],
+                num_attempts: 0,
+                time: currentTimestamp,
+                type: 'firstOpen',
+                stimulus_doc_id: docId,
+                status: 'pending',
+                reattempt: false
+            })
+        })
+        .then(() => {
+            return db.collection('alarms_db').add({
+                timestamp: currentTimestamp,
+                user_id: user_id,
+                reason: "User has opened application for the first time.",
+            })
         })
     })
 
