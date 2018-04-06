e0e1b8dfa8787186a3a079f9a978e1cc74c30451
Joe Breda <joebreda@1x-nat-vl931-172-30-191-11.wireless.umass.edu>
2018-04-06 08:30:27 -0400

    “appRemove on Fri Apr  6 08:30:27 EDT 2018

---
+++ src/firebase_functions/functions/appRemove/stimulus.f.js
@@ -0,0 +1,51 @@
+const functions = require('firebase-functions');
+const curl = require('curlrequest');
+const admin = require('firebase-admin');
+const util = require('util');
+const request = require('request-promise');
+const crypto = require('crypto');
+const sortObj = require('sort-object');
+try {admin.initializeApp(functions.config().firebase);} catch(e) {}
+ // You do that because the admin SDK can only be initialized once.
+
+//Creating a firebase object to navigate it:
+var db = admin.firestore();
+var FieldValue = admin.firestore.FieldValue;
+
+exports = module.exports = functions.firestore
+    .document('app_remove/{docId}').onCreate((event)=>{
+        const docId = event.params.docId
+        const data = event.data.data()
+        const user_id = data.user_id
+        const amount = data.amount
+        const imei = data.imei
+        const token = data.token
+        const currentTimestamp = new Date().getTime()
+        console.log(`The docId of the creation was: ${util.inspect(docId)}`)
+
+        var user = db.collection('user_list').doc(user_id)
+
+        return user.get().then((doc) => {
+            if(doc.data().exists){
+                return db.collection('user_list').doc(user_id).update({
+                    active: false
+                })
+                .then(() => {
+                    return db.collection('user_activity').add({
+                        user_id: user_id,
+                        active: false,
+                        timestamp: currentTimestamp
+                    })
+                })
+                .then(() => {
+                    return db.collection('user_timers').doc(user_id).update({
+                        lastTimeActive: currentTimestamp,
+                        active: false
+                    })
+                })
+            } else {
+                console.log('user does not exist...')
+                return
+            }
+        })
+    })
