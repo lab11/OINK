83c42cc91a0db260e7b086fdfad24188bf7d7346
santiagocorrea <santiago.correa.cardona@gmail.com>
2018-04-11 23:14:05 -0400

    Migration to v1.0+ on Wed Apr 11 23:14:05 EDT 2018

--- src/firebase_functions/functions/cron/checker.f.js
+++ src/firebase_functions/functions/cron/checker.f.js
@@ -6,7 +6,7 @@ const util = require('util');
 const request = require('request-promise');
 const crypto = require('crypto');
 const sortObj = require('sort-object');
-try {admin.initializeApp(functions.config().firebase);} catch(e) {}
+try {admin.initializeApp();} catch(e) {}
  // You do that because the admin SDK can only be initialized once.
 
 //Creating a firebase object to navigate it:
