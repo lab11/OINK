f8acb62e6b1554c5eb5c76f5d096d51aea64964f
santiagocorrea <santiago.correa.cardona@gmail.com>
2018-04-16 17:07:41 -0400

    Adding notification on Mon Apr 16 17:07:41 EDT 2018

--- src/firebase_functions/functions/oink/core2.f.js
+++ src/firebase_functions/functions/oink/core2.f.js
@@ -12,8 +12,12 @@ try {admin.initializeApp();} catch(e) {}
 var db = admin.firestore();
 var FieldValue = admin.firestore.FieldValue;
 
-
-
+//Core2 Function:
+// - Triggers on a callback from Korba API which sends a get request to the URL of Core2. 
+//   The get request contains the transaction ID, status ("SUCCESS/FAILED") an a message about the transaction.
+// - Using the transaction ID, we are able to trace the transaction. This function logs on rx_core_payment
+//   the result of the transaction, if successful sends notification to user, otherwise sends an alarm to the
+//   system admin. This function also updates the stimulus_transaction and tx_core_payment status.
 
 exports = module.exports = functions.https
     .onRequest((req, res) => {
@@ -25,34 +29,26 @@ exports = module.exports = functions.https
         var userId_doc;
         var stimulus_doc;
         var msgs_doc;
+
         console.log(util.inspect(req.query));
-        // return db.collection('rx_core_payment').add({
-        //     timestamp: FieldValue.serverTimestamp(),
-        //     //type: docData.type,
-        //     //amount: docData.amount,
-        //     user_id: req.query.transaction_id.slice(0,-9),
-        //     transaction_id: req.query.transaction_id,
-        //     status: req.query.status,
-        //     message: req.query.message
         
-        // })
-        
+        //Getting the document in tx_core that matches the transaction id and updating the variables.
         return db.collection('tx_core_payment').where('transaction_id','==', req.query.transaction_id).get()
         .then(snapshot =>{
             
-                //Calculating the total num of invites that the specific user has sent.
-                snapshot.forEach(doc => {
-                    tx_core_doc_id = doc.id;
-                    amount_doc = doc.data().amount;
-                    type_doc = doc.data().type;
-                    userId_doc = doc.data().user_id;
-                    stimulus_doc = doc.data().stimulus_doc_id;
-                    msgs_doc = doc.data().msgs;
-                    console.log(doc.id, " => ", doc.data());
-                        
-                });
+            snapshot.forEach(doc => {
+                tx_core_doc_id = doc.id;
+                amount_doc = doc.data().amount;
+                type_doc = doc.data().type;
+                userId_doc = doc.data().user_id;
+                stimulus_doc = doc.data().stimulus_doc_id;
+                msgs_doc = doc.data().msgs;
+                console.log(doc.id, " => ", doc.data());
+                    
+            });
                 
         })
+        //Logging on rx_core the result of the transaction
         .then(() => {
             return db.collection('rx_core_payment').add({
                 timestamp: FieldValue.serverTimestamp(),
@@ -70,25 +66,31 @@ exports = module.exports = functions.https
         })
         
         .then(() => {
+            //If confirmation from Korba successful, write on notification_db that triggers function of
+            //user notification.
             if (req.query.status == 'SUCCESS') {
                 return db.collection('notifications_db').add({
                     amount: amount_doc,
                     type: type_doc,
                     status: 'success',
-                    timestamp: new Date().getTime(),
+                    timestamp: FieldValue.serverTimestamp(),
                     body: `Your ${type_doc} transaction has been submitted for ${amount_doc} CHD. Thank you!`,
-                    title:"Transaction submitted",
+                    title:"Transaction completed.",
                     user_id: userId_doc
                 })
+                //Updating stimulus_transaction status
                 .then(() => {
                     return db.collection(`${type_doc}_transaction`).doc(stimulus_doc).update({status: 'complete'})
                 })
+                //Updating tx_core status
                 .then(() => {
                     return db.collection('tx_core_payment').doc(tx_core_doc_id).update({status: 'complete'})
                 })
                 
 
             }
+            //If confirmation from Korba has fail status, write on alarms_db that triggers function to 
+            //send alarm to system admin.
             else {
                 return db.collection('alarms_db').add({
                     timestamp: FieldValue.serverTimestamp(),
