cf592de50b1468689616c3de222790ea7f1d7077
santiagocorrea <santiago.correa.cardona@gmail.com>
2018-04-16 18:06:57 -0400

    Adding temp logs function for tx_core on Mon Apr 16 18:06:57 EDT 2018

---
+++ src/firebase_functions/functions/logs/tx.f.js
@@ -0,0 +1,51 @@
+const functions = require('firebase-functions');
+const curl = require('curlrequest');
+const admin = require('firebase-admin');
+const util = require('util');
+const request = require('request-promise');
+const crypto = require('crypto');
+const sortObj = require('sort-object');
+try {admin.initializeApp();} catch(e) {}
+ // You do that because the admin SDK can only be initialized once.
+
+//Creating a firebase object to navigate it:
+var db = admin.firestore();
+var FieldValue = admin.firestore.Field
+
+//Function logsTx:
+//Triggers when tx_core_payment changes. 
+exports = module.exports = functions.firestore
+    .document('tx_core_payment/{docId}').onUpdate((change, context) =>{
+        //Getting the data that was modified and initializing all the parameters for payment.
+        const data = change.after.data();
+        const previousData = change.before.data();
+        const docId = context.params.docId;
+        
+        //send the request to server to log the change
+        return request({
+            uri: 'http://graphs.grid.watch:3245',
+     
+            method: 'POST',
+            headers:{
+                'Content-Type':'application/json',
+            },
+            json: true,
+            body: {
+                    collection: 'tx_core_payment',
+                    transaction_id: data.transaction_id, 
+                    type: data.type,
+                    timestamp: new Date().getTime()
+                },
+            resolveWithFullResponse: true,
+        }).then( response =>{
+            if (response.statusCode >= 400) {
+                console.log(`HTTP Error: ${response.statusCode}`);
+            }
+            console.log('Response body: ', response.body);
+            console.log('Status code: ', response.statusCode);
+        })
+        .catch(err =>{
+            console.log(err);
+        })
+        
+});
