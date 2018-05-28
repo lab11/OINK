d4ef4e8fef012dc8827de3aa89022b3476fb7786
santiagocorrea <santiago.correa.cardona@gmail.com>
2018-05-28 07:36:16 -0400

    tmp type change for payments on Mon May 28 07:36:16 EDT 2018

--- src/firebase_functions/functions/oink/core2.f.js
+++ src/firebase_functions/functions/oink/core2.f.js
@@ -81,7 +81,7 @@ exports = module.exports = functions.https
                 //Updating stimulus_transaction status
                 .then(() => {
                     //TODO:return db.collection(`${type_doc}_transaction`).doc(stimulus_doc).update({status: 'complete'})
-                    return db.collection(firstOpen_transaction).doc(stimulus_doc).update({status: 'complete'})
+                    return db.collection('firstOpen_transaction').doc(stimulus_doc).update({status: 'complete'})
                 })
                 //Updating tx_core status
                 .then(() => {
@@ -100,7 +100,7 @@ exports = module.exports = functions.https
                     tx_core_doc_id:tx_core_doc_id })
                 .then(() => {
                     //TODO:return db.collection(`${type_doc}_transaction`).doc(stimulus_doc).update({status: 'failed'})
-                    return db.collection(firstOpen_transaction).doc(stimulus_doc).update({status: 'failed'})
+                    return db.collection('firstOpen_transaction').doc(stimulus_doc).update({status: 'failed'})
                 })
                 .then(() => {
                     return db.collection('tx_core_payment').doc(tx_core_doc_id).update({status: 'failed', msgs:msgs_doc.push('transaction error')})
