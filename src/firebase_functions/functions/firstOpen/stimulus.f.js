9954a7bc507ee2a32de3acb9584d004debd87187
santiagocorrea <santiago.correa.cardona@gmail.com>
2018-06-04 17:24:03 -0400

    update for checker support on Mon Jun  4 17:24:03 EDT 2018

--- src/firebase_functions/functions/firstOpen/stimulus.f.js
+++ src/firebase_functions/functions/firstOpen/stimulus.f.js
@@ -14,18 +14,6 @@ try {admin.initializeApp();} catch(e) {}
 var db = admin.firestore();
 var FieldValue = admin.firestore.FieldValue;
 
-//stimulus_firstOpen funtion:
-/* 
-1.  user opens app -> creates firstOpen document in firebase
-2.  firebase function checks user_list for user_id
-3.1 if( user_id present in user_list and active ) {no nothing}
-3.2 if( user_id present in user_list and inactive ) {set to active}
-3.3 if( user_id not present in user_list ) {add to user_list and add to tx_core_payment}
-
-add try catches
-break out into two functions one for queueing and one for paying
-should I be using .then calls for things that are independent 
-*/
 
 exports = module.exports = functions.firestore
     .document('firstOpen_transaction/{docId}').onCreate((snap, context)=>{
@@ -39,10 +27,9 @@ exports = module.exports = functions.firestore
         console.log(`The docId of the creation was: ${util.inspect(docId)}`)
 
         return db.collection('firstOpen_transaction').doc(docId).update({
-            user_activity: "active",
             amount: amount,
-            time_processed: currentTimestamp,
-            processed: true
+            time_processed: currentTimestamp
+            
         })
         .then(() => {
             return db.collection('tx_core_payment').add({
@@ -61,7 +48,7 @@ exports = module.exports = functions.firestore
             return db.collection('alarms_db').add({
                 timestamp: currentTimestamp,
                 user_id: user_id,
-                reason: "User has opened application for the first time.",
+                reason: "User is being incentivized for First Open event.",
             })
         })
     })
