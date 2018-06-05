6c6635ca2af723a1799e7328f3e349cba92ff36b
santiagocorrea <santiago.correa.cardona@gmail.com>
2018-06-05 12:19:46 -0400

    add comments on Tue Jun  5 12:19:46 EDT 2018

--- src/firebase_functions/functions/cron/activityTracker.f.js
+++ src/firebase_functions/functions/cron/activityTracker.f.js
@@ -14,16 +14,30 @@ try {admin.initializeApp();} catch(e) {}
 var db = admin.firestore();
 var FieldValue = admin.firestore.FieldValue;
 
+/*
+cronActivityTracker function:
+- Triggers on creation of a new document in user_activity collection.
+- user_activity collection logs every change in the status of the app: active(installed) and inactive (app removed)
+- cronActivity Tracker  calculates the elapsed time of the user using the app when the app is removed and updates the user timers.
+
+- Parameters:
+    * There are not specific parameters for this function.
+*/
+
 exports = module.exports = functions.firestore
     .document('user_activity/{docId}').onCreate((snap, context) => {
         //Getting the data that was modified and initializing all the parameters.
         const data = snap.data();
         const docId = context.params.docId;
         var docRef = db.collection('user_timers').doc(data.user_id);
+        
+        //declaring temporary variables to update in user_timers
         var tempLastTimeActive;
         var tempElapsedTime;
         var newElapsedTime; 
 
+        //If the user_activity event is the user becoming inactive (app removed),
+        // calculates the time elapsed and update user_timers doc
         if (!data.active) {
 
             return docRef.get()
@@ -32,35 +46,32 @@ exports = module.exports = functions.firestore
                     tempLastTimeActive = doc.data().lastTimeActive;
                     tempElapsedTime = doc.data().elapsedTime;
                     newElapsedTime = tempElapsedTime + (data.timestamp - tempLastTimeActive); 
-                    console.log(tempLastTimeActive)
-                    console.log(tempElapsedTime)
-                    console.log(newElapsedTime)
                 }
                 else {
                     console.log('No such document!');
                 }
             })
             .then(() => {
-                console.log(tempLastTimeActive)
-                console.log(tempElapsedTime)
-                console.log(newElapsedTime)
-
                 return docRef.update({
                     active: false,
                     elapsedTime: newElapsedTime,
                     lastTimeActive: data.timestamp
-
                 })
             })
             .catch( err => {
                 console.log(err);
             });
         }
+        // If the user_activity event is user becoming active (installing for first time or reinstalling the app),
+        // update the active flag in user_timer doc and log its timestamp.
         else { 
             return docRef.update({
                 active: true,
                 lastTimeActive: data.timestamp 
             })
+            .catch( err => {
+                console.log(err);
+            });
         }
 
     });
