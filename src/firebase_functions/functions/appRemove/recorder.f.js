31ef22756e6f4223e57a9d721aeabad0756ebfcc
Pat Pannuto <pat.pannuto@gmail.com>
2018-06-11 02:12:06 +0000

    Try recording all app_remove events on Mon Jun 11 02:12:06 GMT 2018

---
+++ src/firebase_functions/functions/appRemove/recorder.f.js
@@ -0,0 +1,19 @@
+const functions = require('firebase-functions');
+const curl = require('curlrequest');
+const admin = require('firebase-admin');
+const util = require('util');
+const request = require('request-promise');
+const crypto = require('crypto');
+const sortObj = require('sort-object');
+try {admin.initializeApp();} catch(e) {}
+ // You do that because the admin SDK can only be initialized once.
+
+//Creating a firebase object to navigate it:
+var db = admin.firestore();
+var FieldValue = admin.firestore.FieldValue;
+
+exports.markUserRemoved = functions.analytics.event('app_remove').onLog(event => {
+    console.log(event.data);
+
+    return db.collection('app_remove').add(event.data);
+});
