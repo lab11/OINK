58de43c74e714fd9132165d14aa42884e5d2a8a5
Pat Pannuto <pat.pannuto@gmail.com>
2018-06-12 23:04:14 +0000

    phone app updated to correct names; match

--- src/firebase_functions/functions/dwapp/handleUserList.f.js
+++ src/firebase_functions/functions/dwapp/handleUserList.f.js
@@ -22,22 +22,22 @@ exports = module.exports = functions.firestore
         const newValue = change.after.data();
 
         // Records coming from the app
-        const user_id = newValue.id;                        // Opaque, consistent id from all records from the phone
+        const user_id = newValue.user_id;                   // Opaque, consistent id from all records from the phone
         const timestamp = newValue.timestamp;               // Timestamp of this record on the phone
-        const fcm_token = newValue.token;                   // Firebase cloud messaging token
         const payment_service = newValue.payment_service;   // The payment service requested by the user
                                                             // In the future custom payments may change parameters
-        const phone_number = newValue.customer_number;      // The network phone number
-        const phone_imei = newValue.imei;                   // The IMEI of the phone if we can get it
-        const phone_carrier = newValue.network_code;        // The carrier / network operator (e.g. MTN, Tigo)
+        const phone_number = newValue.phone_number;         // The network phone number
+        const phone_imei = newValue.phone_imei;             // The IMEI of the phone if we can get it
+        const phone_carrier = newValue.phone_carrier;       // The carrier / network operator (e.g. MTN, Tigo)
+        const fcm_token = newValue.fcm_token;               // Firebase cloud messaging token
         //console.log({
         //    user_id: user_id,
         //    timestamp: timestamp,
-        //    fcm_token: fcm_token
         //    payment_service: payment_service,
         //    phone_number: phone_number,
         //    phone_imei: phone_imei,
         //    phone_carrier: phone_carrier,
+        //    fcm_token: fcm_token
         //});
 
         // Check if this user already exists
@@ -48,8 +48,8 @@ exports = module.exports = functions.firestore
                 var to_set = {
                     active: true,
                 };
-                if (token != undefined) {
-                    to_set.token = token;
+                if (fcm_token != undefined) {
+                    to_set.fcm_token = fcm_token;
                 }
                 console.log("User '" + user_id + "' already exists. Setting: %j", to_set);
                 return db.collection('OINK_user_list').doc(user_id).update(to_set);
@@ -67,7 +67,6 @@ exports = module.exports = functions.firestore
                     incentivized: incentivized,
                     user_id: user_id,
                     timestamp: timestamp,
-                    fcm_token: fcm_token,
                     payment_service: payment_service,
                     phone_number: phone_number,
                     phone_imei: phone_imei,
