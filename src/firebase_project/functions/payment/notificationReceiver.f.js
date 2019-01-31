8fcdae97ff0f8db3d67d16612828e6cc8f04b5dc
Josh Adkins <adkinsjd@umich.edu>
2019-01-30 16:26:56 -0800

    add a basic notification receiver

---
+++ src/firebase_project/functions/payment/notificationReceiver.f.js
@@ -0,0 +1,16 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const util = require('util');
+try {admin.initializeApp();} catch(e) {}
+ // You do that because the admin SDK can only be initialized once.
+
+//Creating a firebase object to navigate it:
+var db = admin.firestore();
+var FieldValue = admin.firestore.FieldValue;
+
+exports = module.exports = functions.https
+    .onRequest((req, res) => {
+        //Log the payments new status I suppose?? I don't really know how the
+        //callback will work
+        console.log(req)
+});
