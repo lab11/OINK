a460126b5e1681d3e5215c4e455e004c47c55d6d
Joshua Adkuins <adkins@berkeley.edu>
2019-02-20 21:14:29 +0000

    this simplifies the first open and first install code

--- src/firebase_project/functions/incentive/userOnCreate.f.js
+++ src/firebase_project/functions/incentive/userOnCreate.f.js
@@ -16,38 +16,23 @@ const incentive = require('./incentive');
 
 exports = module.exports = functions.firestore
     .document('OINK_user_list/{user_id}')
-    .onUpdate((change, context) => {
-        // Don't care about delete events
-        const before = change.before.exists;
-        const after = change.after.exists;
-        if (before === true && after === false) {
-            return null;
-        }
-
-        const user_id = context.params.user_id
-        const newValue = change.after.data();
-        const previousValue = change.before.data();
-
-        const timestamp = FieldValue.serverTimestamp();
-
+    .onCreate((snapshot, context) => {
         // Collection of things to do
         var todo = []
 
-        //Okay if a user is updated, we should just try to pay them again
-        //This is a theme
-        //The idea is that if something like your phone number changes
-        //We need to catch up on all the payments you missed
-        //If some other inconsequential field changes - well payments
-        //are idempotent because they are IDd by phone number and people
-        //won't be double paid for the same incentive
+        const user_id = context.params.user_id
+        const data = snapshot.data();
+        const timestamp = FieldValue.serverTimestamp();
+
+        //Okay the possible creation states are:
 
         //created with just an app install -> Pay them for first install
-        if (newValue.incentivized && newValue.app_installed) {
+        if (data.incentivized && data.app_installed) {
             todo.push(incentive.incentivize_once(user_id, timestamp, 'firstOpen', INCENTIVE_FIRSTOPEN_AMOUNT));
         }
 
         //created with an app install and powerwatch -> incentivize them for both
-        if (newValue.incentivized &&  newValue.powerwatch_installed){
+        if (data.incentivized &&  data.powerwatch_installed){
             todo.push(incentive.incentivize_once(user_id, timestamp, 'firstPowerwatch', INCENTIVE_FIRSTPOWERWATCH_AMOUNT));
         }
 
